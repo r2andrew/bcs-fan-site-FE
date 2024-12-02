000c79702f3a1c030c68757ad14095d511b1497f
r2andrew <abradley4646@gmail.com>
2024-12-02 17:14:10 +0000

    sort episodes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,9 +2,17 @@ import jsonData from '../assets/episodes.json'
 
 export class DataService {
   pageSize: number = 3;
+
   getEpisodes(page: number) {
+
     let pageStart = (page - 1) * this.pageSize;
     let pageEnd = pageStart + this.pageSize;
+    jsonData.sort((a,b) => {
+      if (a.seasonNumber !== b.seasonNumber) {
+        return a.seasonNumber - b.seasonNumber
+      }
+      return a.episodeNumber - b.episodeNumber
+    })
     return jsonData.slice(pageStart, pageEnd);
   }
 
