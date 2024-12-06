0289430c84df0447db17601c423e6a584fc80af7
r2andrew <abradley4646@gmail.com>
2024-12-06 14:20:08 +0000

    use ag-grid

--- src/app/episodes.component.ts
+++ src/app/episodes.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule} from '@angular/router';
 import { DataService } from './data.service';
 import { CommonModule } from '@angular/common';
 import { WebService } from './web.service';
 import jsonData from '../assets/episodes.json';
+import {AgGridAngular} from 'ag-grid-angular';
+import { ColDef, GridOptions } from 'ag-grid-community';
 
 
 @Component({
   selector: 'episodes',
-  standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, AgGridAngular],
   providers: [DataService, WebService],
   templateUrl: './episodes.component.html',
   styleUrl: './episodes.component.css'
@@ -22,10 +23,41 @@ export class EpisodesComponent {
   totalPages: number = 1;
   episodes_loaded: boolean = false;
 
+  headings: ColDef[] = [
+    { field: 'title',
+          filter: true,
+          floatingFilter: true,
+          sortable: false,
+          },
+    { field: 'imdbRating',
+          headerName: 'Rating' },
+    { headerName: 'Episode',
+          valueGetter: p => 'S' + p.data.seasonNumber + 'E' + p.data.episodeNumber,
+          sortable: false }
+  ]
+
+  gridOptions: GridOptions = {
+    rowStyle: {cursor: 'pointer', backgroundColor: 'rgba(0,0,0,0.5)'},
+    autoSizeStrategy: {type: 'fitProvidedWidth', width: 700},
+    getRowStyle: (params: any) => {
+      // Get current page number
+      const pageSize = 17;
+      const pageIndex = params.api.paginationGetCurrentPage();  // Gets the current page index
+
+      // Get the row index in the current page context
+      const rowIndexOnPage = params.rowIndex - pageIndex * pageSize;
+
+      // Check if the current row is one of the last 3 rows on the current page
+      if (rowIndexOnPage >= pageSize - 6 && rowIndexOnPage < pageSize && rowIndexOnPage % 2 == 0) {
+        return {backgroundColor: 'rgba(255,255,255,0.85)', color: 'black'};  // Apply custom class
+      }
+    }
+  }
 
   protected readonly Math = Math;
   constructor(public dataService: DataService,
-              private webService: WebService) {}
+              private webService: WebService,
+              private router: Router) {}
 
   ngOnInit() {
     if (sessionStorage['page']) {
@@ -43,6 +75,10 @@ export class EpisodesComponent {
         this.episodes_list = this.data.slice(pageStart, pageEnd)
         this.totalPages = Math.ceil( this.data.length / this.pageSize )
       })
+  }
+
+  onRowClicked(event: any) {
+    this.router.navigate(['/episodes/' + event.data._id])
   }
 
   previousPage() {
