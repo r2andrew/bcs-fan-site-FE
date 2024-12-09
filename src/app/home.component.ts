f5390bc5f58b1f0d5c9be9b05c58e7c01d90097a
r2andrew <abradley4646@gmail.com>
2024-12-09 13:41:32 +0000

    homepage

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {NgForOf} from "@angular/common";
+import {RouterModule} from '@angular/router';
 @Component({
     selector: 'home',
     standalone: true,
     templateUrl: './home.component.html',
     imports: [
-        NgForOf
+        NgForOf, RouterModule
     ],
     styleUrl: './home.component.css'
 })
